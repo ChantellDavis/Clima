c08c4515ed5b33bb4bab4074028ff39587a8f8f3
ChantellDavis <155310121+ChantellDavis@users.noreply.github.com>
2024-04-28 12:56:20 -0400

    added index.js

---
+++ src/index.js
@@ -0,0 +1,34 @@
+
+
+function formatDate(date) {
+    let day = days[date.getDay()];
+let hours = date.getHours();
+let minutes = date.getMinutes();
+
+if (hours > 10) {
+    let hours = `0${hours}`
+};
+
+if (minutes > 10) {
+    let minutes = `0${minutes}`
+};
+
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+
+let formattedDate = days[day];
+return `${formattedDate}, ${hours};${minutes}`;
+
+}
+
+let currentDate = document.querySelector("#current-date-time");
+let now = new Date();
+currentDate.innerHTML = formatDate(now);
